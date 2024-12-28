176b416e4ba645691f6f4c66f1ae5a29d486c6a3
Staffeya <rutkevicsviktors@gmail.com>
2024-12-28 19:56:43 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+﻿function App() {
+    return (
+        <div>
+            <h1>Добро пожаловать в мини-приложение!</h1>
+            <p>Заполните анкету и найдите своё совпадение.</p>
+            <form>
+                <label>
+                    Имя:
+                    <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                    Возраст:
+                    <input type="number" name="age" />
+                </label>
+                <br />
+                <label>
+                    Интересы:
+                    <input type="text" name="interests" />
+                </label>
+                <br />
+                <button type="submit">Отправить</button>
+            </form>
+        </div>
+    );
+}
+
+export default App;
